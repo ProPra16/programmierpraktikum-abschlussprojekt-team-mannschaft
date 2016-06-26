a318e16a9043813224c44280e6d524e830769bb2
RoterEmu <roter-emu@online.de>
2016-06-26 14:02:29 +0200

    vorläufiges GUI - Style wird noch gemacht

--- src/main/java/tddt/gui/Controller.java
+++ src/main/java/tddt/gui/Controller.java
@@ -1,6 +1,7 @@
 package main.java.tddt.gui;
 
 
+import javafx.event.ActionEvent;
 import javafx.scene.layout.GridPane;
 
 public class Controller {
@@ -8,4 +9,39 @@ public class Controller {
 
     public GridPane terminalpane;
 
+    public void selectExercise(ActionEvent actionEvent) {
+    }
+
+    public void openExercise(ActionEvent actionEvent) {
+    }
+
+    public void runTest(ActionEvent actionEvent) {
+    }
+
+    public void saveProject(ActionEvent actionEvent) {
+    }
+
+    public void babysteps(ActionEvent actionEvent) {
+    }
+
+    public void createExercise(ActionEvent actionEvent) {
+    }
+
+    public void showLog(ActionEvent actionEvent) {
+    }
+
+    public void deleteLog(ActionEvent actionEvent) {
+    }
+
+    public void showTerminal(ActionEvent actionEvent) {
+    }
+
+    public void goBack(ActionEvent actionEvent) {
+    }
+
+    public void undo(ActionEvent actionEvent) {
+    }
+
+    public void handleTerminal(ActionEvent actionEvent) {
+    }
 }
