44e92167f56376ffce0154233246cb31f29028c1
RoterEmu <roter-emu@online.de>
2016-06-29 10:33:50 +0200

    aktueller Stand gui

--- src/main/java/tddt/gui/Controller.java
+++ src/main/java/tddt/gui/Controller.java
@@ -2,12 +2,24 @@ package main.java.tddt.gui;
 
 
 import javafx.event.ActionEvent;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import main.java.tddt.gui.dialogs.BabystepsDialog;
 
 public class Controller {
 
-
-    public GridPane terminalpane;
+    public Label phaselabel;
+    public Label clockpic;
+    public Label clock;
+    public TextArea classpane;
+    public TextArea testpane;
+    public Label consolepane;
+    public Label descriptionpane;
+    public Label graphpane;
+    public SplitPane mainSplitPane;
+    private Stage stage;
 
     public void selectExercise(ActionEvent actionEvent) {
     }
@@ -22,6 +34,10 @@ public class Controller {
     }
 
     public void babysteps(ActionEvent actionEvent) {
+        try {
+            BabystepsDialog dialog = new BabystepsDialog(stage);
+        }
+        catch(Exception e){e.printStackTrace();}
     }
 
     public void createExercise(ActionEvent actionEvent) {
@@ -33,9 +49,6 @@ public class Controller {
     public void deleteLog(ActionEvent actionEvent) {
     }
 
-    public void showTerminal(ActionEvent actionEvent) {
-    }
-
     public void goBack(ActionEvent actionEvent) {
     }
 
@@ -43,5 +56,29 @@ public class Controller {
     }
 
     public void handleTerminal(ActionEvent actionEvent) {
+        double[] pos = this.mainSplitPane.getDividerPositions();
+        if(pos[0] < 0.99){
+            this.mainSplitPane.setDividerPosition(0,1.0);
+        }
+        else{
+            this.mainSplitPane.setDividerPosition(0,0.6);
+        }
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void nextPhase(ActionEvent actionEvent) {
+    }
+
+    public void close(ActionEvent actionEvent) {
+        stage.close();
+    }
+
+    public void maximize(ActionEvent actionEvent) {
+    }
+
+    public void minimize(ActionEvent actionEvent) {
     }
 }
--- src/main/java/tddt/gui/GUIMain.java
+++ src/main/java/tddt/gui/GUIMain.java
@@ -2,36 +2,32 @@ package main.java.tddt.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.*;
-import javafx.stage.Screen;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import main.java.tddt.gui.Controller;
+import javafx.stage.StageStyle;
 
-public class GUIMain extends Application {
+public class GUIMain extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("gui.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/gui.fxml"));
         Parent root = loader.load();
         Controller controller = (Controller) loader.getController();
+        controller.setStage(primaryStage);
         Scene scene = new Scene(root);
-        String stylesheet = getClass().getResource("gui.css").toExternalForm();
+        String stylesheet = getClass().getResource("resources/gui.css").toExternalForm();
         scene.getStylesheets().add(stylesheet);
+        primaryStage.getIcons().add(new Image(GUIMain.class.getResourceAsStream("resources/graphics/tddt.png")));
         primaryStage.setMaximized(true);
         primaryStage.setTitle("Test Driven Development Trainer");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public static void launchApp(String[] args){
+    public static void launchGUI(String[] args){
         launch(args);
     }
+
 }
