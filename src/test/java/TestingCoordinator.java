ffd2af259ed4ede49df0a287710b340b77c9de49
haftbefehl <renato-vukovic@uni-duesseldorf.de>
2016-07-01 10:47:36 +0200

    phasenwechseltests und einige aenderungen am Coordinator.compile

--- src/test/java/TestingCoordinator.java
+++ src/test/java/TestingCoordinator.java
@@ -8,12 +8,70 @@ import org.junit.*;
 public class TestingCoordinator {
     @Test
     public void vonphase1zu2Test(){ //testet, ob die phasen richtig gewechselt werden, hier von  ohase 1 zu 2
-        Coordinator coor = new Coordinator("Testklasse", "Testtest");
+        Coordinator coor = new Coordinator("Bar", "BarTest");
         //für die Bedingung muss es fehlschlagende Tests geben
-        String klassencontent = "public class Testklasse{ static int ret2(){return 1;}}";
-        String testcontent = "public class Testest{ @Test public void testeret2(){ assertEquals(Testklasse.ret2(), 2)}}";
-        coor.compile(klassencontent, testcontent);
-        assertEquals(coor.phase, 2); //sollte danach dann 2 sein
+        //Testklassenstrings aus den Tests der Biblithek entnommen
+        String klassencontent = "public class Bar { \n"
+                + " public static int square(int n) { \n"
+                + "    return n * n; \n"
+                + " }\n"
+                + " public static int plusThree(int n) { \n"
+                + "    return 3 + n; \n"
+                + " }\n"
+                + "}";
+        String testcontent = "import static org.junit.Assert.*;\n"
+                + "import org.junit.Test;\n"
+                + "public class BarTest { \n"
+                + "   @Test\n"
+                + "   public void squareOf2_shouldReturn16() { \n "
+                + "       assertEquals(16, Bar.square(2)); \n"
+                + "   }\n "
+                + "}";
+        String testcompresults = coor.compile(klassencontent, testcontent);
+        assertEquals(2, coor.phase); //sollte danach dann 2 sein
+    }
+
+    @Test
+    public void vonphase2zu3Test(){ //testet ob bei erfolgreichen Tests in phase 2 zu phase 3 übergegangen wird
+        Coordinator coortest = new Coordinator("Bar", "BarTest");
+        //Testklassenstrings aus der Compile Bibiltohek
+        String klassencontentfalsch = "public class Bar { \n"
+                + " public static int square(int n) { \n"
+                + "    return n * n; \n"
+                + " }\n"
+                + " public static int plusThree(int n) { \n"
+                + "    return 3 + n; \n"
+                + " }\n"
+                + "}";
+        String testcontentfalsch = "import static org.junit.Assert.*;\n"
+                + "import org.junit.Test;\n"
+                + "public class BarTest { \n"
+                + "   @Test\n"
+                + "   public void squareOf2_shouldReturn16() { \n "
+                + "       assertEquals(16, Bar.square(2)); \n"
+                + "   }\n "
+                + "}";
+        String failetestresults = coortest.compile(klassencontentfalsch, testcontentfalsch);
+        //phase sollte hiernach 2 sein
+        //Testklassenstrings aus den Tests der Biblithek entnommen
+        String klassencontent = "public class Bar { \n"
+                + " public static int square(int n) { \n"
+                + "    return n * n; \n"
+                + " }\n"
+                + " public static int plusThree(int n) { \n"
+                + "    return 3 + n; \n"
+                + " }\n"
+                + "}";
+        String testcontent = "import static org.junit.Assert.*;\n"
+                + "import org.junit.Test;\n"
+                + "public class BarTest { \n"
+                + "   @Test\n"
+                + "   public void squareOf3_shouldReturn9() { \n "
+                + "       assertEquals(9, Bar.square(3)); \n"
+                + "   }\n "
+                + "}";
+        String testcompresults = coortest.compile(klassencontent, testcontent);
+        assertEquals(3, coortest.phase); //phase sollte danach dann 3 sein
     }
 
 }
