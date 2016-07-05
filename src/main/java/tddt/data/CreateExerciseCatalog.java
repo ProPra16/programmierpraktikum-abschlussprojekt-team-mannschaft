7cd8de5c769e50803a96c644adb2fc891222ffbd
RoterEmu <roter-emu@online.de>
2016-07-06 00:41:52 +0200

    kleine Klasse um den Exercise Katalog lokal zu speichern, damit er editierbar wird

---
+++ src/main/java/tddt/data/CreateExerciseCatalog.java
@@ -0,0 +1,30 @@
+package main.java.tddt.data;
+
+import main.java.tddt.gui.dialogs.CreateExerciseController;
+
+import java.io.*;
+
+/**
+ * Created by Roter Emu on 05.07.2016.
+ */
+public class CreateExerciseCatalog {
+
+    public CreateExerciseCatalog(File resFile, File catalogFile) throws Exception {
+        File[] files = resFile.listFiles();
+        catalogFile.mkdirs();
+        for (File file : files) {
+            copyFile(file, new File(catalogFile.getAbsolutePath() + System.getProperty("file.separator") + file.getName()));
+        }
+    }
+
+    private static void copyFile(File file, File dest) throws Exception {
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(dest, true));
+        int bytes = 0;
+        while ((bytes = in.read()) != -1) {
+            out.write(bytes);
+        }
+        in.close();
+        out.close();
+    }
+}
