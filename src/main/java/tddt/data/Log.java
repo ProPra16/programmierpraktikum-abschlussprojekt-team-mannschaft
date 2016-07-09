814d0c951634a9a0715e8f0ee155ea0888dc7ec4
sameh <samou101@uni-duesseldorf.de>
2016-07-09 13:38:05 +0200

    Änderung zu Log und LogList

--- src/main/java/tddt/data/Log.java
+++ src/main/java/tddt/data/Log.java
@@ -1,86 +1,84 @@
 package main.java.tddt.data;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.time.LocalDate;
+import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+// ---> Zustand der Arbeit des Users (es wird gespeichert was gerade vom User geänder wurde)
 @XmlRootElement(name="log")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Log  { // ---> Zustand der Arbeit des Users (es wird gespeichert was gerade vom User geänder wurde)
+public class Log  {
 
-    private  int phase;
-    private String  time;
-    private  String timer; // Hier sind alle Instanzvariablen der Klasse
-    private  String  classText;
+    private int phase;
+    private String time;
+    private String timer;
+    private String classText;
     private String testText;
     private String compileMessage;
 
-    public Log() {
-
-    }
-
-    public Log (int phase, LocalDateTime time, LocalDateTime timer, String classText, String testText, String compileMessage){
+    // Konstruktor
+    public Log (int phase, LocalDateTime time, LocalDateTime timer,
+                String classText, String testText, String compileMessage) {
         this.phase = phase;
+
+        // setze Time und Timer
         this.setTime(time);
         this.setTimer(timer);
-        this.classText = classText ;
-        this.testText = testText ;
+
+        this.classText = classText;
+        this.testText = testText;
         this.compileMessage= compileMessage;
     }
-    public int getPhase(){
-        return this.phase;
+
+    // erstelle Log-XML Datei
+    public static void createLog(int phase, LocalDateTime time, LocalDateTime timer,
+                                 String classText, String testText, String compileMessage,
+                                 File file) throws JAXBException {
+        JAXBContext creation = JAXBContext.newInstance(Log.class);
+        Marshaller marshaller = creation.createMarshaller();
+
+        // Erstelle XML Datei und Dateiname ist Datum und Uhr
+        marshaller.marshal(new Log(phase, time, timer, classText, testText, compileMessage), new File(file + File.separator, time.toString()));
+    }
+
+    public void setPhase(int phase) { this.phase=phase; }
+    public void setTime(LocalDateTime time) { this.time = time.toString(); }
+    public void setTimer(LocalDateTime timer) { this.timer = timer.toString(); }
+    public void setClassText(String classText) { this.classText= testText;}
+    public void setTestText(String testText) { this.testText = testText; }
+    public void setCompileMessage(String compileMessage) { this.compileMessage = compileMessage; }
+
+
+    // lesen Log-XML Datei
+    public static Log getLog(File file) throws JAXBException {
+        JAXBContext creation = JAXBContext.newInstance(Log.class);
+        Unmarshaller unmarshaller = creation.createUnmarshaller();
+        return (Log) unmarshaller.unmarshal(file);
     }
 
     public LocalDateTime getTime() {
-        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        LocalDateTime dateTime = LocalDateTime.parse(time, timeFormat);
-
-        return dateTime;
+        // z.B. 2016-07-04T16:30:42.576
+        // jahr-monat-tagTstunden:minute:sekunde.milisekunde
+        String str ="2016-07-04T16:30:42.576";
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss.zzz");
+        return LocalDateTime.parse(str, timeFormat);
 
     }
 
     public LocalDateTime getTimer() {
-        DateTimeFormatter timerFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        LocalDateTime dateTimer = LocalDateTime.parse(timer, timerFormat);
-
-        return dateTimer;
-    }
-    public String getClassText(){
-        return this.classText;
-    }
-    public String getTestText(){
-        return this.testText;
-    }
-    public String getCompileMessage(){
-        return this.compileMessage;
+        DateTimeFormatter timerFormat = DateTimeFormatter.ofPattern("HH-mm-ss");
+        return LocalDateTime.parse(timer, timerFormat);
     }
 
-
-    public void setPhase(int phase){
-        this.phase=phase;
-    }
-
-    public void setTime(LocalDateTime time) {
-
-        this.time = time.toString().replaceAll("T"," ");
-    }
-
-    public void setTimer(LocalDateTime timer) {
-        this.timer = timer.toString().replaceAll("T"," ");
-    }
-
-    public void setClassText(String classText){
-        this.classText= testText;
-    }
-    public void setTestText(String testText){
-        this.testText = testText;
-    }
-    public void setCompileMessage(String compileMessage){
-        this.compileMessage = compileMessage;
-    }
+    public int getPhase() { return this.phase; }
+    public  String getClassText() { return this.classText; }
+    public String getTestText() { return this.testText; }
+    public String getCompileMessage() { return this.compileMessage; }
 }
--- src/main/java/tddt/data/LogList.java
+++ src/main/java/tddt/data/LogList.java
@@ -1,76 +1,88 @@
 package main.java.tddt.data;
 
-
-
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.*;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
 
-
-@XmlRootElement(name="logList")
-@XmlAccessorType (XmlAccessType.FIELD)
 public class LogList {
 
-    @XmlElement(name = "log")
-    private List<Log> logs = new ArrayList<Log>();
+    // Liste mit allen logs
+    List<Log> logs = new ArrayList<>();
 
+    // Pfad zu exercises-Ordner wo alle XML Datein gesaved werden
+    public static String directoryPath = LogList.class.getResource("exercices/").toExternalForm();
 
-    public List<Log> getLogs() {
-        return logs;
+    // Konstruktor
+    // geht zu Ordner-exercises und sucht alle Datei mit XML
+    // und fuegt dann alle in die obere Liste
+    public LogList() {
+        // oeffne exercises-Ordner
+        File directory = new File(directoryPath);
+
+        // suche alle Datein mit Endung XML und save Dateinamen in Array
+        String[] allLogFiles = directory.list((File dir, String name) -> name.endsWith(".xml"));
+
+        // oeffne alle gefundenen Datein und lese XML und fuege in logs Liste hinzu
+        Log log = null;
+        JAXBContext creation = null;
+        for (String logFile : allLogFiles) {
+            try {
+                creation = JAXBContext.newInstance(Log.class);
+                Unmarshaller unmarshaller = creation.createUnmarshaller();
+                log = (Log) unmarshaller.unmarshal(getClass().getResource("/exercises/" + logFile));
+                logs.add(log);
+            } catch (JAXBException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // fuege log in Liste und erstelle neue log-XML Datei
+    public void addLog(Log log) throws JAXBException {
+        logs.add(log);
+        Log.createLog(log.getPhase(), log.getTime(), log.getTimer(), log.getClassText(), log.getTestText(), log.getCompileMessage(),new File(directoryPath));
+    }
+
+    // delete alle Logs aus logs-Liste und alle Datein aus exercises-Ordner
+    public void deleteAll() {
+        for (Log log : logs) {
+            deleteLast();
+        }
+    }
+
+    // delete letzte Log aus logs-List und aus exercises-Ordner
+    public void deleteLast() {
+        // speichere Name von letzten Log aus der logs-List
+        String fileNameLastLog = logs.get(logs.size() - 1).getTime().toString();
+
+        // delete letzte Log aus der logs-Liste
+        logs.remove(logs.size() - 1);
+
+        // oeffne letzen Log-Datei aus exercises-Ordner
+        File fileLastLog = new File(directoryPath + fileNameLastLog);
+
+        // Delete letzte Log-Datei und gibt Meldung aus
+        if (fileLastLog.delete() == true) System.out.println(fileLastLog.getName() + "wurde deleted");
+        else {
+            System.out.println("Fehler beim Log-deleten");
+        }
     }
 
     public void setLogs(List<Log> logs) {
         this.logs = logs;
     }
 
-    public void addLog(Log log){
-        logs.add(log);
-        saveLoglist();
+    public Log getLog(int index) {
+        return logs.get(index);
     }
 
-    public void delete(){
-        logs.clear();
-        saveLoglist();
-    }
+    public List<Log> getLogs() {
+        return logs;
 
-    public Log getLog(int i){
-        return logs.get(i);
-    }
-
-
-    public void loadLoglist(){
-        try {
-            JAXBContext context = JAXBContext.newInstance(LogList.class);
-
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-
-            LogList loglist = (LogList) unmarshaller.unmarshal(new File("Log.xml"));
-            this.logs = loglist.getLogs();
-        }catch(JAXBException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
-
-    public void saveLoglist(){
-        try {
-            JAXBContext context = JAXBContext.newInstance(LogList.class);
-
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            marshaller.marshal(this,new File("Log.xml"));
-
-        }catch(JAXBException e){
-            e.printStackTrace();
-        }
     }
 }
